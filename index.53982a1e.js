812531f2b284d058d5ffeb04f573f647b268ebc1
Nadiia <koropchenkon@gmail.com>
2026-02-10 15:44:22 +0200

    make build

---
+++ index.53982a1e.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var t=document.createElement("label");t.classList.add("field-label"),t.htmlFor=e.id,t.textContent=e.name;var a=e.name[0].toUpperCase()+e.name.slice(1);e.placeholder=a,e.parentElement.append(t)});
+//# sourceMappingURL=index.53982a1e.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const allInputs = document.querySelectorAll("form input");
+allInputs.forEach((input)=>{
+    const label = document.createElement("label");
+    label.classList.add("field-label");
+    label.htmlFor = input.id;
+    label.textContent = input.name;
+    const placeholderText = input.name[0].toUpperCase() + input.name.slice(1);
+    input.placeholder = placeholderText;
+    // eslint-disable-next-line no-shadow
+    const parent = input.parentElement;
+    parent.append(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
